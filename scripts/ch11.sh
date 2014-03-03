637399b096567920408a48545d461c2d6402ffc3
Trey Grainger <code@treygrainger.com>
2014-03-02 20:04:04 -0500

    Adding chapter 11 examples to scripts/

--- scripts/ch11.sh
+++ scripts/ch11.sh
@@ -4,15 +4,54 @@ if [ "$#" -ne 2 ]; then
 fi
 SOLR_IN_ACTION=${1%/}
 SOLR_INSTALL=${2%/}
-kill -9 $(ps aux | grep '[j]ava -jar start.jar' | awk '{print $2}') #stops Solr if running from previous chapter
-sleep 2 #give process time to stop
-
+for ID in `ps waux | grep java | grep [s]tart.jar | awk '{print $2}' | sort -r`
+  do
+    kill -9 $ID
+    echo "Killed previous Solr process: $ID"
+done #stops Solr if running from previous chapter
+sleep 2 #give processes time to die
 echo -e "----------------------------------------\n"
 echo -e "CHAPTER 11"
 echo -e "----------------------------------------\n"
 echo -e "\n\n"
-echo -e "pg xxx"
+echo -e "pg 333"
 echo -e "\n"
 cd $SOLR_INSTALL/example/
-java -jar start.jar &
+cp -r $SOLR_IN_ACTION/example-docs/ch11/cores/ecommerce/ solr/ecommerce/
+echo -e "Starting Solr example server on port 8983; see $SOLR_INSTALL/example/solr.log for errors and log messages"
+java -jar start.jar 1>solr.log 2>&1 &
 sleep 10 #give Solr time to start
+cd $SOLR_IN_ACTION/example-docs/
+java -Durl=http://localhost:8983/solr/ecommerce/update -jar post.jar ch11/documents/ecommerce.xml
+echo -e "\n"
+echo -e "pg 333"
+echo -e "\n"
+java -jar $SOLR_IN_ACTION/solr-in-action.jar listing 11.3
+echo -e "\n"
+echo -e "pg 336"
+echo -e "\n"
+java -jar $SOLR_IN_ACTION/solr-in-action.jar listing 11.4
+echo -e "\n"
+echo -e "pg 338"
+echo -e "\n"
+java -jar $SOLR_IN_ACTION/solr-in-action.jar listing 11.5
+echo -e "\n"
+echo -e "pg 340"
+echo -e "\n"
+java -jar $SOLR_IN_ACTION/solr-in-action.jar listing 11.6
+echo -e "\n"
+echo -e "pg 343"
+echo -e "\n"
+java -jar $SOLR_IN_ACTION/solr-in-action.jar listing 11.7
+echo -e "\n"
+echo -e "pg 345"
+echo -e "\n"
+java -jar $SOLR_IN_ACTION/solr-in-action.jar listing 11.8
+echo -e "\n"
+echo -e "pg 349"
+echo -e "\n"
+java -jar $SOLR_IN_ACTION/solr-in-action.jar listing 11.9
+echo -e "\n"
+echo -e "pg 351"
+echo -e "\n"
+java -jar $SOLR_IN_ACTION/solr-in-action.jar listing 11.10
