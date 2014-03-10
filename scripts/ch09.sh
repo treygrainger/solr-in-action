2e2d6a79acd815c4e8715f4c0d62aab9b512b92f
Trey Grainger <code@treygrainger.com>
2014-03-10 00:25:51 -0400

    Creating ufo core automatically so the script doesn't require manual intervention by the reader to run it

--- scripts/ch09.sh
+++ scripts/ch09.sh
@@ -1,78 +1,88 @@
 if [ "$#" -ne 2 ]; then
-  echo -e "Usage: ch9.sh \$SOLR_IN_ACTION \$SOLR_INSTALL"
+  echo -e "Usage: ch09.sh \$SOLR_IN_ACTION \$SOLR_INSTALL"
   exit 0
 fi
 SOLR_IN_ACTION=${1%/}
 SOLR_INSTALL=${2%/}
-for ID in `ps waux | grep java | grep start.jar | awk '{print $2}' | sort -r`
+for ID in `ps waux | grep java | grep [s]tart.jar | awk '{print $2}' | sort -r`
   do
     kill -9 $ID
-    echo "Killed process $ID"
-done
+    echo "Stopped previous Solr process: $ID"
+done #stops Solr if running from previous chapter
 echo -e "----------------------------------------\n"
 echo -e "CHAPTER 9"
 echo -e "----------------------------------------\n"
-echo -e "\n\n"
 echo -e "pg 284"
 echo -e "\n"
 
-cd $SOLR_INSTALL/example/
-echo -e "Starting Solr example server on port 8983; see $SOLR_INSTALL/example/solr.log for errors and log messages"
-java -jar start.jar 1>solr.log 2>&1 &
-sleep 10 
-tail -30 solr.log
-
-# chapter 9 relies on the user manually creating the ufo core
-if [ ! -d "$SOLR_INSTALL/example/solr/ufo" ]; then
-  echo -e "\n\n"
-  echo -e "pg 284"
-  echo -e "\n"
-  cd $SOLR_INSTALL/example/solr
-  cp -r collection1 ufo
-  rm -rf ufo/data
-  rm ufo/core.properties
-  echo -e "\n\n"
-  echo -e "Manually create the ufo core as documented in chapter 9 and then re-run this script."
-  exit 0
+# chapter 9 expects the user manually creating the ufo core,
+# but this script will do that automatically to run the examples
+if [ -d "$SOLR_INSTALL/example/solr/ufo" ]; then
+  echo -e "Deleting pre-existing 'ufo' core to start fresh."
+  rm -rf "$SOLR_INSTALL/example/solr/ufo" 
 fi
 
-echo -e "\n\n"
+cd $SOLR_INSTALL/example/solr
+cp -r collection1 ufo
+rm -rf ufo/data
+rm ufo/core.properties
+
+# enabling the core before starting Solr has same effect as the 
+# instructions on pg 285 to create the core from the Solr Admin page
+echo -e "Creating a new 'ufo' core automatically (instead of manually, as instructed in chapter 9)."
+touch ufo/core.properties 
+
+cd $SOLR_INSTALL/example/
+echo -e "Starting Solr example server on port 8983; see $SOLR_INSTALL/example/solr.log for errors and log messages"
+java -Xmx512m -jar start.jar 1>solr.log 2>&1 &
+sleep 10 
+
+echo -e "\n"
 echo -e "pg 286"
 echo -e "\n"
 
-# TODO: Need to set the path to the ufo_awesome.json file after downloading/extracting as documented in chapter 9
-UFO_DATA_JSON=
+# Note: If you downloaded the ufo_awesome.json file to somewhere other 
+# than the book's suggested location, you should modify the UFO_DATA_JSON 
+# variable below to the location of the ufo_awesome.json file after 
+# downloading/extracting as documented in chapter 9
+UFO_DATA_JSON="/tmp/chimps_16154-2010-10-20_14-33-35/ufo_awesome.json"
 
-if [ "$UFO_DATA_JSON" == "" ] || [ ! -e $UFO_DATA_JSON ]; then
+if [ ! -e $UFO_DATA_JSON ]; then
   echo -e "UFO data $UFO_DATA_JSON file not found!"
-  echo -e "Please download the UFO data set from http://www.infochimps.com/datasets/60000-documented-ufo-sightings-with-text-descriptions-and-metada"
-  echo -e "After downloading, extract the zip to a temp directory and set the UFO_DATA_JSON variable in this script to the ufo_awesome.json file."
-  echo -e "\n\n"
+  echo -e "Please download the UFO data set from http://www.infochimps.com/datasets/60000-documented-ufo-sightings-with-text-descriptions-and-metada."
+  echo -e "After downloading, extract the zip to a temp directory such as $UFO_DATA_JSON."
+  echo -e "If you download to a different location, you can update the UFO_DATA_JSON variable in this script to point to your copy of the ufo_awesome.json file."
+  echo -e "\n"
   exit 1
 fi
 
 java -jar $SOLR_IN_ACTION/solr-in-action.jar ufo -jsonInput $UFO_DATA_JSON
-echo -e "\n\n"
+echo -e "\n"
 echo -e "pg 288"
 echo -e "\n"
 java -jar $SOLR_IN_ACTION/solr-in-action.jar listing 9.1 
-echo -e "\n\n"
+echo -e "\n"
 echo -e "pg 289"
 echo -e "\n"
 java -jar $SOLR_IN_ACTION/solr-in-action.jar listing 9.2 
-echo -e "\n\n"
+echo -e "\n"
 echo -e "pg 296"
 echo -e "\n"
 java -jar $SOLR_IN_ACTION/solr-in-action.jar listing 9.4 
-echo -e "\n\n"
+echo -e "\n"
 echo -e "pg 297"
 echo -e "\n"
 java -jar $SOLR_IN_ACTION/solr-in-action.jar listing 9.5 
-echo -e "\n\n"
+echo -e "\n"
 echo -e "pg 298"
 echo -e "\n"
 java -jar $SOLR_IN_ACTION/solr-in-action.jar listing 9.6 
-echo -e "\n\n"
+echo -e "\n"
 echo -e "pg 301"
 echo -e "\n"
 java -jar $SOLR_IN_ACTION/solr-in-action.jar listing 9.7 
+for ID in `ps waux | grep java | grep [s]tart.jar | awk '{print $2}' | sort -r`
+  do
+    kill -9 $ID
+    echo "Stopped Solr process: $ID"
+done
